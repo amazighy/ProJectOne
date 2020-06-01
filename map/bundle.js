dd6205e156237ce047719dfee769183823e13c71
amazigh <amazigh@ubuntu.fios-router.home>
2020-06-01 15:16:18 -0400

    June Update

--- map/bundle.js
+++ map/bundle.js
@@ -103,74 +103,79 @@
   const pathGenerator = d3.geoPath().projection(projection);
 
   const choroplethMap = (selection, props) => {
-      const {features,
-          colorScale,
-          colorValue,
-          selectedColorValue,
-          onCountryClick
-      } = props;
+    const {
+      features,
+      colorScale,
+      colorValue,
+      selectedColorValue,
+      onCountryClick,
+    } = props;
 
-    
+    const gUpdate = selection.selectAll("g").data([null]);
+    const gEnter = gUpdate.enter().append("g");
+    const g = gUpdate.merge(gEnter);
 
-  const gUpdate = selection.selectAll('g').data([null]);
-  const gEnter = gUpdate.enter().append('g');
-  const g= gUpdate.merge(gEnter);
+    selection.call(
+      d3.zoom().on("zoom", () => {
+        g.attr("transform", d3.event.transform);
+      })
+    );
+    const countryPaths = g.selectAll("path").data(features);
 
+    const Title = selection
+      .append("text")
+      .attr("class", "Title")
+      .attr("y", 35)
+      .attr("x", 350)
+      .attr("fill", "black")
+      .text("Confirmed COVID-19 deaths: June, 1st");
 
-      selection.call(d3.zoom().on('zoom', () => {
-      g.attr('transform', d3.event.transform);
-    }));
-  const countryPaths= g.selectAll('path')
-          .data(features);
+    const countryPathsEnter = countryPaths
+      .enter()
+      .append("path")
+      .attr("class", "country")
+      .attr("transform", `translate(-45,100)`);
 
-   const Title= selection.append('text')
-          .attr('class', 'Title')
-          .attr('y',35)
-          .attr('x', 350)
-          .attr('fill', 'black')
-          .text('Confirmed COVID-19 deaths: May, 6th');
-
-  const countryPathsEnter=countryPaths
-      .enter().append('path')
-          .attr('class', 'country')
-          .attr('transform', `translate(-45,100)`);
-      const tipF = d3.tip()
-      .attr('class', 'tip card')
-      .html(d => {
-          let content = `<div class="name">${d.properties.location }</div>`;
-          content += `<div class="cost"> Total Deaths: ${+d.properties.total_deaths}</div>`;
-          content += `<div class="cost"> Daily Deaths: ${+d.properties.new_deaths}</div>`;
-          content += `<div class="cost"> New  Cases: ${+d.properties.new_cases}</div>`;
-          content += `<div class="cost"> Total Cases: ${+d.properties.total_cases}</div>`;
-          content += `<div class="more">Click for details</div>`;
-          return content ;
+    const tipF = d3.tip()
+      .attr("class", "tip card")
+      .html((d) => {
+        let content = `<div class="name">${d.properties.location}</div>`;
+        content += `<div class="cost"> Total Deaths: ${+d.properties
+        .total_deaths}</div>`;
+        content += `<div class="cost"> Daily Deaths: ${+d.properties
+        .new_deaths}</div>`;
+        content += `<div class="cost"> New  Cases: ${+d.properties
+        .new_cases}</div>`;
+        content += `<div class="cost"> Total Cases: ${+d.properties
+        .total_cases}</div>`;
+        content += `<div class="more">Click for details</div>`;
+        return content;
       });
 
-  countryPathsEnter.call(tipF);
-      
+    countryPathsEnter.call(tipF);
 
-  countryPaths    
+    countryPaths
       .merge(countryPathsEnter)
-          .attr('d', pathGenerator)
-          .attr('fill',  d=> colorScale(colorValue(d)))
-          .attr('opacity', d=>
-          (!selectedColorValue || selectedColorValue=== colorValue(d))
-          ? 1
-          : 0.2
-          )
-          .classed('highlighted',d=> 
-          selectedColorValue && selectedColorValue === colorValue(d))
-          .on('click', (d) =>{
-              onCountryClick(d);
-          });
+      .attr("d", pathGenerator)
+      .attr("fill", (d) => colorScale(colorValue(d)))
+      .attr("opacity", (d) =>
+        !selectedColorValue || selectedColorValue === colorValue(d) ? 1 : 0.2
+      )
+      .classed(
+        "highlighted",
+        (d) => selectedColorValue && selectedColorValue === colorValue(d)
+      )
+      .on("click", (d) => {
+        onCountryClick(d);
+      });
 
-      countryPathsEnter
-          .on('mouseover', (d,i,n) => {
-          tipF.show(d, n[i]);
-          })
-          .on('mouseout', (d) => {
-              tipF.hide();
-          });
+    countryPathsEnter
+      .on("mouseover", (d, i, n) => {
+        tipF.show(d, n[i]);
+      })
+      .on("mouseout", (d) => {
+        tipF.hide();
+      });
   };
 
   const margin = { top: 40, right: 30, bottom: 40, left: 50 };
